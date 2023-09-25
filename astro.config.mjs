023eb0447e549f7bc7039001bf4536758c427dd5
Samin Yaser <saminyaserwork@gmail.com>
2023-09-25 20:26:03 +0600

    added netlify, node version

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
-
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
   integrations: [mdx(), tailwind({
     applyBaseStyles: false
-  }), compress(), react()]
+  }), compress(), react()],
+  output: "server",
+  adapter: netlify()
 });
