9f060596be08b9fcb96c00cd8a7821615c141d25
Samin Yaser <saminyaserwork@gmail.com>
2024-02-23 15:39:30 +0600

    seo meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,29 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
-import compress from 'astro-compress';
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import tailwind from '@astrojs/tailwind'
+import compress from 'astro-compress'
+import react from '@astrojs/react'
+import vercel from '@astrojs/vercel/serverless'
 
-import vercel from "@astrojs/vercel/serverless";
+import metaTags from 'astro-meta-tags'
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  integrations: [mdx(), tailwind({
-    applyBaseStyles: false
-  }), compress(), react()],
+  integrations: [
+    mdx(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    compress(),
+    react(),
+    metaTags(),
+  ],
   vite: {
     ssr: {
-      noExternal: ["react-icons"]
-    }
+      noExternal: ['react-icons'],
+    },
   },
-  output: "server",
-  adapter: vercel()
-});
+  output: 'server',
+  adapter: vercel(),
+})
