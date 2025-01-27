04b9798fecc1ebdcdbeeae07314bd7a5e5d8f74d
Samin Yaser <saminyaserwork@gmail.com>
2025-01-27 12:59:10 +0600

    basic working build

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,12 @@
+
+export default new Map([
+["src/content/blog/Generating Word Cloud from Bengali Text using Python.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2FGenerating+Word+Cloud+from+Bengali+Text+using+Python.mdx&astroContentModuleFlag=true")],
+["src/content/blog/Typesafe Wordpress Block Attributes.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2FTypesafe+Wordpress+Block+Attributes.mdx&astroContentModuleFlag=true")],
+["src/content/project/agrosmart.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproject%2Fagrosmart.mdx&astroContentModuleFlag=true")],
+["src/content/project/autoturret.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproject%2Fautoturret.mdx&astroContentModuleFlag=true")],
+["src/content/project/instaclone.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproject%2Finstaclone.mdx&astroContentModuleFlag=true")],
+["src/content/project/newsflash.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproject%2Fnewsflash.mdx&astroContentModuleFlag=true")],
+["src/content/project/opengl-adventures.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproject%2Fopengl-adventures.mdx&astroContentModuleFlag=true")],
+["src/content/project/quran-cli.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproject%2Fquran-cli.mdx&astroContentModuleFlag=true")],
+["src/content/project/traffichain.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproject%2Ftraffichain.mdx&astroContentModuleFlag=true")]]);
+		
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,40 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
-import compress from 'astro-compress'
-import react from '@astrojs/react'
-import vercel from '@astrojs/vercel/serverless'
+/** @format */
 
-import metaTags from 'astro-meta-tags'
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel";
+import compress from "astro-compress";
+import icon from "astro-icon";
+import metaTags from "astro-meta-tags";
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
-  compressHTML: true,
-  integrations: [
-    mdx(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    compress(),
-    react(),
-    metaTags(),
-  ],
-  vite: {
-    ssr: {
-      noExternal: ['react-icons'],
+    compressHTML: true,
+    integrations: [
+        mdx(),
+        tailwind({
+            applyBaseStyles: false,
+        }),
+        compress(),
+        react(),
+        metaTags(),
+        icon({
+            include: {
+                ic: ["*"],
+                "fa-solid": ["user-graduate"],
+                teenyicons: ["school-solid"],
+                ep: ["school"],
+            },
+        }),
+    ],
+    vite: {
+        ssr: {
+            noExternal: ["react-icons"],
+        },
     },
-  },
-  output: 'server',
-  adapter: vercel({
-    imageService: true,
-  }),
-})
+    output: "server",
+    adapter: vercel({
+        imageService: true,
+    }),
+});
