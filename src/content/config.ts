acbc96a615bb6549e7b1c006df193b84debbb29a
Samin Yaser <saminyaserwork@gmail.com>
2026-07-10 21:36:23 +0600

    added projects

--- src/content/config.ts
+++ src/content/config.ts
@@ -16,17 +16,23 @@ const blogCollection = defineCollection({
 const projectCollection = defineCollection({
   type: 'content',
   schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      tags: z.array(z.string()),
-      image: image().optional(),
-      description: z.string(),
-      date: z.coerce.date(),
-      ghUrl: z.string(),
-      liveUrl: z.string().optional(),
-      draft: z.boolean().optional().default(true),
-      featured: z.boolean().default(false),
-    }),
+    z
+      .object({
+        title: z.string(),
+        tags: z.array(z.string()),
+        image: image().optional(),
+        description: z.string(),
+        date: z.coerce.date(),
+        ghUrl: z.string().optional(),
+        bbUrl: z.string().optional(),
+        wpUrl: z.string().optional(),
+        liveUrl: z.string().optional(),
+        draft: z.boolean().optional().default(true),
+        featured: z.boolean().default(false),
+      })
+      .refine((project) => project.ghUrl || project.bbUrl, {
+        message: 'A GitHub or Bitbucket source URL is required',
+      }),
 })
 
 export const collections = {
